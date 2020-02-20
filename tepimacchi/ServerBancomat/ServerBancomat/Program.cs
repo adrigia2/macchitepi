f169cf33ae0c1adbfea48fa79c88a881bcec1ea8
adria <adria@DESKTOP-D9IN97S>
2020-02-20 14:06:46 +0100

    modifiche varie

--- tepimacchi/ServerBancomat/ServerBancomat/Program.cs
+++ tepimacchi/ServerBancomat/ServerBancomat/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,25 @@ namespace ServerBancomat
     {
         static void Main(string[] args)
         {
+            //ottiene l'indirizzo ip dell'host specificato (host locale)
+            IPAddress[] ip = Dns.GetHostAddresses(Dns.GetHostName());
+
+            //si mette in ascolto in rete per le richieste di connessione dai client
+            TcpListener s = new TcpListener(ip[0], 10100);
+            //avvia l'ascolto delle richieste di connessione in ingresso
+            s.Start();
+            //conta i clienti serviti
+            int contaClient = 0;
+            while (true)
+            {
+                Console.WriteLine("Sono il SERVER e sono in attesa di connessione...");
+                //accetta una richiesta di connessione in sospeso
+                TcpClient conn = s.AcceptTcpClient();
+                contaClient++; //incremento il numero di client connessi
+                Console.WriteLine("Client numero " + contaClient + "connesso!");
+                new Server(conn, contaClient); //per ogni richiesta di connessione crea un nuovo thread (vedi
+                                               //costruttore classe Server)
+            }
         }
     }
 }
---
+++ tepimacchi/ServerBancomat/ServerBancomat/Server.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerBancomat
+{
+    class Server
+    {
+        private TcpClient dati;
+        private int numClient;
+        public Server(TcpClient dati, int i)
+        {
+            this.dati = dati;
+            this.numClient = i;
+            //thread che si prenderà carico della richiesta del client
+            Thread th = new Thread(new ThreadStart(this.Attività));
+            th.Start(); //il thread si autolancia
+        }
+        public void Attività()
+        {
+            Byte[] buf = new byte[256]; //buffer ricezione messaggio
+            String msgIn = null;
+            //NetworkStream usato per inviare e rivevere il flusso di dati (byte)
+            NetworkStream s = dati.GetStream();
+            int i;
+            Console.WriteLine("Attivo thread numero " + numClient);
+            while ((i = s.Read(buf, 0, buf.Length)) != 0) //legge finchè ci sono dati
+            {
+                msgIn = System.Text.Encoding.ASCII.GetString(buf, 0, i);
+                Console.WriteLine("Frase ricevuta dal client: " + msgIn);
+                msgIn = msgIn.ToUpper();
+                byte[] msgOut = System.Text.Encoding.ASCII.GetBytes(msgIn);
+                s.Write(msgOut, 0, msgOut.Length);
+            }
+            s.Close();
+            dati.Close();
+
+        }
+    }
+}
